ea146e3d74668c1f8af5807c93731796b4f7da37
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-05-12 22:08:38 +0530

    Factorization

--- project1.js
+++ project1.js
@@ -1,32 +1,21 @@
-console.log("Welcome to Practice Problem!!");
-function generateRandomNumbers() {
-    let randomNumbers = [];
-    for (let i = 0; i < 10; i++) {
-      let randomNumber = Math.floor(Math.random() * 900) + 100;
-      randomNumbers.push(randomNumber);
+function primeFactorization(n) {
+    let factors = [];
+    let divisor = 2;
+  
+    while (n > 2) {
+      if (n % divisor == 0) {
+        factors.push(divisor);
+        n = n / divisor;
+      } else {
+        divisor++;
+      }
     }
-    return randomNumbers;
-  }
-
   
-  function findSecondLargestAndSmallest(numbers) {
-    let sortedNumbers = numbers.slice().sort((a, b) => a - b); // Sort the numbers array in ascending order
-     console.log("sorted Random Number",sortedNumbers);
-  let secondLargest = sortedNumbers[sortedNumbers.length - 2]; // Get the second largest element
-  let secondSmallest = sortedNumbers[1]; // Get the second smallest element
-
-   
-  
-    return {
-      secondLargest,
-      secondSmallest
-    };
+    return factors;
   }
   
-  let randomNumbers = generateRandomNumbers();
-  console.log("Random Numbers:", randomNumbers);
-  
-  let results = findSecondLargestAndSmallest(randomNumbers);
-  console.log("Second Largest:", results.secondLargest);
-  console.log("Second Smallest:", results.secondSmallest);
+ 
+  let number = 84;
+  let primeFactors = primeFactorization(number);
+  console.log("Prime Factors of", number + ":", primeFactors);
   
