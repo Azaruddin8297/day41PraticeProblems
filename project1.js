193c1ee6e57dd28bb9195fc87148cbd9fffa6d7c
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-05-12 22:05:17 +0530

    CollectRandom

--- project1.js
+++ project1.js
@@ -1,1 +1,45 @@
 console.log("Welcome to Practice Problem!!");
+function generateRandomNumbers() {
+    let randomNumbers = [];
+    for (let i = 0; i < 10; i++) {
+      let randomNumber = Math.floor(Math.random() * 900) + 100;
+      randomNumbers.push(randomNumber);
+    }
+    return randomNumbers;
+  }
+  
+  function findSecondLargestAndSmallest(numbers) {
+    let largest = numbers[0];
+    let secondLargest = -Infinity;
+    let smallest = numbers[0];
+    let secondSmallest = Infinity;
+  
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] > largest) {
+        secondLargest = largest;
+        largest = numbers[i];
+      } else if (numbers[i] > secondLargest && numbers[i] < largest) {
+        secondLargest = numbers[i];
+      }
+  
+      if (numbers[i] < smallest) {
+        secondSmallest = smallest;
+        smallest = numbers[i];
+      } else if (numbers[i] < secondSmallest && numbers[i] > smallest) {
+        secondSmallest = numbers[i];
+      }
+    }
+  
+    return {
+      secondLargest,
+      secondSmallest
+    };
+  }
+  
+  let randomNumbers = generateRandomNumbers();
+  console.log("Random Numbers:", randomNumbers);
+  
+  let results = findSecondLargestAndSmallest(randomNumbers);
+  console.log("Second Largest:", results.secondLargest);
+  console.log("Second Smallest:", results.secondSmallest);
+  
